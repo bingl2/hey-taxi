cf425f79cd4d33d99b1243431dba55ce324d3605
Hyunbin.Choi <chlgusrla@gmail.com>
2019-06-29 23:02:42 +0900

    controller 구조 잡기

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::UserController < ApiController
+  swagger_controller :users, "User Management"
+
+  swagger_api :index do
+    summary "Fetches all User items"
+    notes "This lists all the active users"
+    param :query, :page, :integer, :optional, "Page number"
+    param :path, :nested_id, :integer, :optional, "Team Id"
+    response :unauthorized
+    response :not_acceptable, "The request you made is not acceptable"
+    response :requested_range_not_satisfiable
+  end
+end
