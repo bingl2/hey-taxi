6a89d2ef54c6d0b889592145e18e501f74acdc39
Hyunbin.Choi <chlgusrla@gmail.com>
2019-06-30 20:38:20 +0900

    공통으로 사용할 api 구현중

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,9 @@
 class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   include Swagger::Docs::ImpotentMethods
 
+  def self.swagger_access_token(api, required = true)
+    api.param :form, :access_token, :long, required ? :required : :optional, "Access Token"
+  end
 end
