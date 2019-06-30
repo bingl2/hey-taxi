885fbe54512fddd48095df222c1df3d0b04d3768
Hyunbin.Choi <chlgusrla@gmail.com>
2019-06-30 21:53:43 +0900

    Type Check 임시 추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   has_one :access_token
 
+  TYPES = {
+      Passenger: '승객',
+      Driver: '기사'
+  }
+
+  def self.type_check(type)
+    TYPES.keys.include?(type.to_sym)
+  end
+
   def get_access_token
     self.access_token ||= AccessToken.create!(user: self)
+
     self.access_token.access_token
   end
 end
