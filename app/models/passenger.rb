c0fb24b015d495beb67266647091af51e65e133e
Hyunbin.Choi <chlgusrla@gmail.com>
2019-06-30 23:24:52 +0900

    모델 기능 추가중

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,12 @@
 class Passenger < User
   has_many :transports, foreign_key: 'passenger_id'
 
+  def transport_request(destination)
+    # TODO 사용중인 request 가 있을 경우 리퀘스트 요청 실패하도록 수정
+
+  end
+
+  def request_able?
+    self.transports.using.not_completed.count == 0
+  end
 end
--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,4 +1,10 @@
 class Transport < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
+
+  scope :using, -> { where(use_yn: 'Y') }
+  scope :not_completed, -> { where.not(status: 'COMPLETED')}
+
+  STATUS = %w(WAIT CANCEL PROGRESS COMPLETED)
+
 end
