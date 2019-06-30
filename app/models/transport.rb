bfb2c4cfe6bab1ee561239b490648eaceafa7043
Hyunbin.Choi <chlgusrla@gmail.com>
2019-06-30 23:28:36 +0900

    스코프랑 기능 잘못걸린거 옮김

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -2,9 +2,4 @@ class Transport < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
 
-  scope :using, -> { where(use_yn: 'Y') }
-  scope :not_completed, -> { where.not(status: 'COMPLETED')}
-
-  STATUS = %w(WAIT CANCEL PROGRESS COMPLETED)
-
 end
--- app/models/transport_history.rb
+++ app/models/transport_history.rb
@@ -1,2 +1,7 @@
 class TransportHistory < ApplicationRecord
+  scope :using, -> { where(use_yn: 'Y') }
+  scope :not_completed, -> { where.not(status: 'COMPLETED')}
+
+  STATUS = %w(WAIT CANCEL PROGRESS COMPLETED)
+
 end
